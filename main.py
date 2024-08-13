4430022f8b4e0f77096610454635278ed8d07099
Natalya Karpova <knataly0513@gmail.com>
2024-08-13 18:24:25 +0200

    add functional
    add functional to ask user if he wants to add new contact

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ from handlers import (
     show_upcoming_birthdays,
     show_all_notes,
     search_contact,
+    add_email_to_contact,
+    add_phone_to_contact,
+    add_address_to_contact,
 )
 from helpers import setup_logging
 from helpers.assistant_info import assistant_info
@@ -56,6 +59,35 @@ def main():
 
         elif command == "add-note":
             print(add_note(notes_book))
+        elif command == "add-contact":
+            contact_name = input("Enter contact's name: ").lower()
+            print(add_contact(contact_name, book))
+            contact_phone = input("Enter contact's phone number: ").lower()
+            print(add_phone_to_contact(contact_name, contact_phone, book))
+
+            email_input = input("Do you want to add email y/n: ").lower()
+            if email_input == "y":
+                contact_email = input("Enter contact's email: ").lower()
+                args = [contact_name, contact_email]
+                print(add_email_to_contact(args, book))
+            if email_input == "n":
+                pass
+
+            birthday_input = input("Do you want to add birthday y/n: ").lower()
+            if birthday_input == "y":
+                contact_birthday = input("Enter contact's birthday: ").lower()
+                args = [contact_name, contact_birthday]
+                print(add_birthday(args, book))
+            if email_input == "n":
+                pass
+
+            address_input = input("Do you want to add address y/n: ").lower()
+            if address_input == "y":
+                contact_address = input("Enter contact's address: ").lower()
+                args = [contact_name, contact_address]
+                print(add_address_to_contact(args, book))
+            if email_input == "n":
+                pass
 
         elif command == "change":
             print(change_contact(args, book))
@@ -83,6 +115,8 @@ def main():
 
         elif command == "search":
             print(search_contact(args, book))
+        elif command == "add-email":
+            print(add_email_to_contact(args, book))
 
         else:
             print("Invalid command.")
