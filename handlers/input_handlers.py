a6233e5977ad208698c3eb29a61ac0639f402e08
Natalya Karpova <knataly0513@gmail.com>
2024-08-14 09:04:13 +0200

    Update

--- handlers/input_handlers.py
+++ handlers/input_handlers.py
@@ -11,6 +11,49 @@ from handlers import (
 )
 
 
+def input_contact_name():
+    contact_name = input("Enter contact's name: ").lower()
+    if len(contact_name) == 0:
+        print("The name must contain at least one symbol")
+        input_contact_name()
+    return contact_name
+
+
+def input_contact_phone():
+    contact_phone = input("Enter contact's phone number: ").lower()
+    # валідація номеру
+    if len(contact_phone) == 10 and contact_phone.isdigit():
+        return contact_phone
+    else:
+        print("The phone number must contain 10 only numbers")
+        input_contact_phone()
+
+
+def input_contact_email():
+    contact_email = input("Enter contact's email: ").lower()
+    if len(contact_email) == 0:
+        print("The email must contain at least 5 symbols")
+        input_contact_email()
+    # тут має бути валідація имейл
+    return contact_email
+
+
+def input_contact_birthday():
+    contact_birthday = input("Enter contact's birthday: ").lower()
+    if len(contact_birthday) == 0:
+        input_contact_birthday()
+    # тут потрібна валідація дня народження
+    # без адекватної валідації буде невірно працювати при невалідному форматі дати
+    return contact_birthday
+
+
+def input_contact_address():
+    contact_address = input("Enter contact's address: ").lower()
+    if len(contact_address) == 0:
+        input_contact_address()
+    return contact_address
+
+
 def add_contact_input(book):
     """An addition input module for adding the first contact,
     his phone number, email, address, birthday
@@ -19,36 +62,12 @@ def add_contact_input(book):
             book (class): contact list
     """
 
-    def input_contact_name():
-        contact_name = input("Enter contact's name: ").lower()
-        if len(contact_name) == 0:
-            print("The name must contain at least one symbol")
-            input_contact_name()
-        return contact_name
-
     contact_name = input_contact_name()
     print(add_contact(contact_name, book))
-
-    def input_contact_phone():
-        contact_phone = input("Enter contact's phone number: ").lower()
-        # валідація номеру
-        if len(contact_phone) == 10 and contact_phone.isdigit():
-            return contact_phone
-        else:
-            print("The phone number must contain 10 only numbers")
-            input_contact_phone()
 
     contact_phone = input_contact_phone()
     args = [contact_name, contact_phone]
     print(add_phone_to_contact(args, book))
-
-    def input_contact_email():
-        contact_email = input("Enter contact's email: ").lower()
-        if len(contact_email) == 0:
-            print("The email must contain at least 5 symbols")
-            input_contact_email()
-        # тут має бути валідація имейл
-        return contact_email
 
     def email_input_y_or_n():
         email_input = input("Do you want to add email y/n: ").lower()
@@ -63,14 +82,6 @@ def add_contact_input(book):
 
     email_input_y_or_n()
 
-    def input_contact_birthday():
-        contact_birthday = input("Enter contact's birthday: ").lower()
-        if len(contact_birthday) == 0:
-            input_contact_birthday()
-        # тут потрібна валідація дня народження
-        # без адекватної валідації буде невірно працювати при невалідному форматі дати
-        return contact_birthday
-
     def birthday_input_y_or_n():
         birthday_input = input("Do you want to add birthday y/n: ").lower()
         if birthday_input != "y" and birthday_input != "n":
@@ -84,12 +95,6 @@ def add_contact_input(book):
             pass
 
     birthday_input_y_or_n()
-
-    def input_contact_address():
-        contact_address = input("Enter contact's address: ").lower()
-        if len(contact_address) == 0:
-            input_contact_address()
-        return contact_address
 
     def address_input_y_or_n():
         address_input = input("Do you want to add address y/n: ").lower()
